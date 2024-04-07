4f74bcffd1f5932a0c98644e83aa2038e94867f6
joeyanovich <joedison.dias@gmail.com>
2024-04-06 22:53:25 -0300

    add 🔨: create conditions with zod lib

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import fastify from "fastify";
+import { z } from 'zod'
 
 const app = fastify()
 
 app.post('/events', (request, reply) => {
-  console.log(request.body)
+  const createEventSchema = z.object({
+    title: z.string().min(4),
+    details: z.string().nullable(),
+    maximumAttendees: z.number().int().positive().nullable(),
+  })
+
+  const data = createEventSchema.parse(request.body)
 
   return 'Hello NLW Unite'
 })
