ecb9093dee50d01195503cf8f0eaac82a251a57a
joeyanovich <joedison.dias@gmail.com>
2024-04-09 23:45:39 -0300

    database 🗃️: add cascades

---
+++ prisma/seed.ts
@@ -0,0 +1,18 @@
+import { prisma } from "../src/lib/prisma";
+
+async function seed() {
+  await prisma.event.create({
+    data: {
+      id: '9897cc61-d7b5-440c-ab9a-49ba741c3025',
+      title: 'Unite Summit',
+      slug:'unite-summit',
+      details: 'Um evento pra devs apaixonados(as) por código!',
+      maximumAttendees: 120,
+    }
+  })
+}
+
+seed().then(() => {
+  console.log('Database seeded!')
+  prisma.$disconnect()
+})
