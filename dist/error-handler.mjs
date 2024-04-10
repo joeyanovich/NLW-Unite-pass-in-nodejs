5ba12bc8b7641164821fc45641bf18bdc75da730
joeyanovich <joedison.dias@gmail.com>
2024-04-10 00:44:16 -0300

    script 🔨: add scripts

---
+++ dist/error-handler.mjs
@@ -0,0 +1,7 @@
+import {
+  errorHandler
+} from "./chunk-UOYF5ETJ.mjs";
+import "./chunk-JRO4E4TH.mjs";
+export {
+  errorHandler
+};
---
+++ dist/server.mjs
@@ -0,0 +1,65 @@
+import {
+  registerForEvent
+} from "./chunk-ELIVTXBS.mjs";
+import {
+  errorHandler
+} from "./chunk-UOYF5ETJ.mjs";
+import {
+  checkIn
+} from "./chunk-EFMAK5UM.mjs";
+import {
+  createEvent
+} from "./chunk-FPNLTCO4.mjs";
+import "./chunk-KDMJHR3Z.mjs";
+import {
+  getAttendeeBadge
+} from "./chunk-L7N3Q3KD.mjs";
+import {
+  getEventAttendees
+} from "./chunk-SUTKDUUT.mjs";
+import {
+  getEvent
+} from "./chunk-AFH345OL.mjs";
+import "./chunk-JRO4E4TH.mjs";
+import "./chunk-JV6GRE7Y.mjs";
+
+// src/server.ts
+import fastify from "fastify";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+import fastifyCors from "@fastify/cors";
+import { serializerCompiler, validatorCompiler, jsonSchemaTransform } from "fastify-type-provider-zod";
+var app = fastify().withTypeProvider();
+app.register(fastifyCors, {
+  origin: "*"
+});
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ["application/json"],
+    produces: ["application/json"],
+    info: {
+      title: "pass.in",
+      description: "Especifica\xE7\xF5es da API para o back-end da aplica\xE7\xE3o pass.in constru\xEDda durante o NLW Unite da Rocketseat.",
+      version: "1.0.0"
+    }
+  },
+  transform: jsonSchemaTransform
+});
+app.register(fastifySwaggerUI, {
+  routePrefix: "/docs"
+});
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+app.register(createEvent);
+app.register(registerForEvent);
+app.register(getEvent);
+app.register(getAttendeeBadge);
+app.register(checkIn);
+app.register(getEventAttendees);
+app.setErrorHandler(errorHandler);
+app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
+  console.log("HTTP server running!");
+});
+export {
+  app
+};
