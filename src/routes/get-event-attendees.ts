27592896769e592060e6d1eec184b349fdc703df
joeyanovich <joedison.dias@gmail.com>
2024-04-10 22:09:45 -0300

    fiz ✏️: fix typo

--- src/routes/get-event-attendees.ts
+++ src/routes/get-event-attendees.ts
@@ -55,7 +55,7 @@ export async function getEventAttendees(app: FastifyInstance) {
               contains: query,
             }
           } : {
-            eventId
+            eventId,
           },
           take: 10,
           skip: pageIndex * 10,
